d166212381c12a20842e0d4346561b6cf1c9728c
Mufadal03 <sadrimufadal@gmail.com>
2023-01-24 17:04:16 +0530

    basic data types

---
+++ Javascript/basic.js
@@ -0,0 +1,8 @@
+"use strict";
+exports.__esModule = true;
+var greetings = "TYPESCRIPT";
+var myNum = 20;
+greetings = greetings.toLowerCase();
+console.log(greetings);
+var result = false;
+console.log(result);
---
+++ Javascript/basic.ts
@@ -0,0 +1,10 @@
+let greetings: string = "TYPESCRIPT"
+let myNum: number = 20 
+greetings = greetings.toLowerCase()
+
+console.log(greetings)
+let result: boolean = false 
+
+console.log(result)
+
+export {}
